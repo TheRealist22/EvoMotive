3ca7475a98b4e892c8c5ece02f4659258c45faaf
TheRealist22 <67223787+TheRealist22@users.noreply.github.com>
2023-05-06 17:48:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// SCROLL ANIMATIONS
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+// PICTURE SLIDESHOW WITH BUTTONS
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("Photos");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+// Animated Sidebar Logo
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
